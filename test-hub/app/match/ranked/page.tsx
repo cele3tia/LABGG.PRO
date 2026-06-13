faa1952ac6ab338e099a95ed11f0f5f9aad4cf58
cele3tia <leehyeon110919@gmail.com>
2026-06-13 23:31:15 +0900

    v1.001

---
+++ test-hub/app/match/ranked/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import React from 'react';
+
+export default function NormalMatchPage() {
+  return (
+    <div className="min-h-screen bg-black text-zinc-500 flex items-center justify-center font-mono text-xs">
+      // TODO: MULTIPLAYER CASUAL MATCHING NODE
+    </div>
+  );
+}
